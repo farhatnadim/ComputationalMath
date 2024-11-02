d99b697c12044e8a6157930e67aa32ec8938daea
Nadim Farhat <nadim.farhat@gmail.com>
2024-11-02 18:55:25 -0400

    solved number 13

--- Linear_Algebra_Strang_fifth_edition/Chapter3/Section3.4_Independence_basis_dimension/Exercises/13.py
+++ Linear_Algebra_Strang_fifth_edition/Chapter3/Section3.4_Independence_basis_dimension/Exercises/13.py
@@ -18,7 +18,7 @@ A_col = sp.Matrix([[1, 1,0,0], [1, 3, 1,0], [3, 1, -1,0]])
 A_t = sp.Matrix([[1,1,3],[1,3,1],[0,1,-1]])
 U_t = sp.Matrix([[1,0,0],[1,2,0],[0,1,0]])
 
-A_row = sp.Matrix([[1,1,3,1],[1,3,1,1],[0,1,-1,0]])
+A_row = sp.Matrix([[1,1,3,0],[1,3,1,0],[0,1,-1,0]])
 # Getting the columspace dimension for U
 # we get the reduced row echelon form and the number of pivots 
 rref_U, pivots = U.rref() 
@@ -31,9 +31,4 @@ print(f"Dimension of the rowspace of Matrix U {len(pivots)}")
 
 print(A_col.rref())
 print(A_row.rref())
-# checking if spaces are the same , is checking if the spaces are a linear combination of each others
-import numpy as np 
-
-A_np = np.array([[1, 1,0], [1, 3, 1], [3, 1, -1]])
-U_np = np.array([[1,1,0],[0,2,1],[0,0,0]])
 
